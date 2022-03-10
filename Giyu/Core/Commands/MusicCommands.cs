73edabdeee15af2e52ef4a8be1690b49c89e6b39
Gabriel <gabriel.silva@ecine.com.br>
2022-03-09 23:14:21 -0300

    Alias duplicado;

--- Giyu/Core/Commands/MusicCommands.cs
+++ Giyu/Core/Commands/MusicCommands.cs
@@ -22,7 +22,7 @@ namespace Giyu.Core.Commands
             => await Context.Channel.SendMessageAsync(embed: await AudioManager.PlayAsync(Context.User as SocketGuildUser, Context.Guild, search, Context));
 
 
-        [Alias("quit", "q")]
+        [Alias("quit", "dc")]
         [Command("leave")]
         [Summary("Faz o bot sair do canal de voz.")]
         public async Task LeaveCommand()
